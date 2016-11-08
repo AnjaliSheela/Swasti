4d34be9ab32b454d6e3cbf9905fa58bd3664da72
P.S.Narayanan <thepsnarayanan@gmail.com>
2016-11-08 18:56:08 +0530

    patch #1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
-
-
-
+    <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,7 +30,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-
+    </head>
 
 @section('content')
 
